4c523b8e99d53def47484678ce5e25ea3322760d
Olorunfemi Ajibulu <FzanyAjibs@gmail.com>
2019-05-27 07:37:51 +0100

    Add project files.

---
+++ Drug/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(window).load(function () {
+    // ADD BOLD ELEMENTS
+    $('.bother:contains("MNOs")').each(function () {
+        $(this).html(
+            $(this).html().replace(/MNOs/g, '<strong>$&</strong>')
+        );
+    });
+    $('.bother:contains("Cable Tv")').each(function () {
+        $(this).html(
+            $(this).html().replace(/Cable Tv/g, '<strong>$&</strong>')
+        );
+    });
+    $('.bother:contains("Power")').each(function () {
+        $(this).html(
+            $(this).html().replace(/Power/g, '<strong>$&</strong>')
+        );
+    });
+});
